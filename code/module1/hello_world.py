d4ecbc1b1310ee12c2640c2bed683aab89c99607
Miguel <miguel.garcia.th@gmail.com>
2020-07-22 18:24:48 +0100

    hello world updates

--- code/module1/hello_world.py
+++ code/module1/hello_world.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a small example to introduce python language.
+- Variable declaration
+- If conditions
+- For / While loops
+- Function defenition
+- Class/Object definition
+"""
 
+one = 1
+two = 2
+three = one + two
+print(three)
 
-print('Hello World')
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
