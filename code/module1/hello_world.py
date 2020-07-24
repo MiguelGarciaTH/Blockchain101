110b3a86b7ea4f695893500701e174c943b41a2c
Miguel <miguel.garcia.th@gmail.com>
2020-07-24 16:04:17 +0100

    First module complete

--- code/module1/hello_world.py
+++ code/module1/hello_world.py
@@ -9,12 +9,43 @@ This is a small example to introduce python language.
 - Class/Object definition
 """
 
+
 one = 1
 two = 2
+three = one + two
+print(three)
+
+one, two = 1 , 2
 three = one + two
 print(three)
 
 hello = "hello"
 world = "world"
 helloworld = hello + " " + world
+print(helloworld)
+
+# Python don't have strong types, but is typed on assigment
+one = 1
+two = 2
+hello = "hello"
+
+#print(one + two + hello) # This do not work
+
+
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
+
+mylist = [1,2,3]
+print(mylist[2])
+
+helloworld = "hello" + " " + "world"
 print(helloworld)
